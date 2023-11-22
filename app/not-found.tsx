7595d068a9da86d437fdd6f389a6f09a8b46074e
Gabriel <gabrielleong917@gmail.com>
2023-11-22 14:57:50 -0800

    finished 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import classes from "./not-found.module.css";
+
+export default function NotFound() {
+  return (
+    <main className={classes.main}>
+      <div className={classes.container}>
+        <h1>Whoops!</h1>
+
+        <Image
+          src="/images/CryingCheems.jpg"
+          alt="404"
+          width={300}
+          height={300}
+          className={classes.image}
+        />
+
+        <h2>We couldn't find that page for you!</h2>
+
+        <Link href="/">Go Back Home</Link>
+      </div>
+    </main>
+  );
+}
